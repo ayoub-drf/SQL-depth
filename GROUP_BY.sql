b86c2219283ccf36059a6de6de65e9bd7d98d2ed
ayoub <ayoubx0x9@gmail.com>
2025-01-18 06:56:23 -0500

    GROUP BY STATEMENT

---
+++ GROUP_BY.sql
@@ -0,0 +1,19 @@
+use sakila;
+
+
+SELECT customer_id, COUNT(rental_id) AS rentals_count FROM rental GROUP BY customer_id;
+
+SELECT customer_id, COUNT(rental_id) FROM rental GROUP BY customer_id ORDER BY COUNT(rental_id) DESC;
+
+SELECT customer_id, SUM(rental_id) AS total_rentals FROM rental GROUP BY customer_id;
+
+SELECT customer_id, MAX(rental_id) AS max_rentals FROM rental GROUP BY customer_id;
+
+SELECT customer_id, MIN(rental_id) AS min_rentals FROM rental GROUP BY customer_id;
+
+SELECT customer_id, AVG(rental_id) AS rental_average FROM rental GROUP BY customer_id;
+
+
+SELECT customer_id, COUNT(rental_id) FROM rental GROUP BY customer_id HAVING COUNT(rental_id) > 33;
+
+
