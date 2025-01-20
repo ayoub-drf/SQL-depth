58cff91051d493753eebe62540a27528cc1b7066
ayoub <ayoubx0x9@gmail.com>
2025-01-20 08:42:46 -0500

    VIEWS

---
+++ Views.sql
@@ -0,0 +1,12 @@
+use my_database;
+
+CREATE VIEW UserBooks AS SELECT `User`.username, `Book`.name FROM `User`
+LEFT JOIN `Book` USING (user_id);
+SELECT * FROM UserBooks;
+
+CREATE OR REPLACE VIEW UserBooks AS SELECT `User`.username, COUNT(`Book`.name) AS Books_count FROM `User`
+LEFT JOIN `Book` USING (user_id) GROUP BY user_id;
+
+
+
+
